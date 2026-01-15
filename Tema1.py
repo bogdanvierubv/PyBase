5a8c395d9d8ac097d39567c4b91276f7e40adf71
bogdanvierubv <bogdan.vieru.bv@gmail.com>
2026-01-16 00:03:18 +0200

    file modified

--- Tema1.py
+++ Tema1.py
@@ -5,19 +5,14 @@ raw_logs= [ " ERROR | Voltage too LOW | code=E12 ", "info | System started succe
            " info | System shutdown complete " ]
 
 clean_string1 = raw_logs[0].strip()
-print(clean_string1)
 print(clean_string1.lower())
 clean_string2 = raw_logs[1].strip()
-print(clean_string2)
 print(clean_string2.lower())
 clean_string3 = raw_logs[2].strip()
-print(clean_string3)
 print(clean_string3.lower())
 clean_string4 = raw_logs[3].strip()
-print(clean_string4)
 print(clean_string4.lower())
 clean_string5 = raw_logs[4].strip()
-print(clean_string5)
 print(clean_string5.lower())
 
 
