1740c7574db6e232316e2e50d4bfc33fb50271ef
bogdanvierubv <bogdan.vieru.bv@gmail.com>
2026-01-16 19:04:32 +0200

    file modified

--- Tema1.py
+++ Tema1.py
@@ -1,4 +1,5 @@
 from copy import replace
+from itertools import count
 
 raw_logs= [ " ERROR | Voltage too LOW | code=E12 ", "info | System started successfully ",
            " WARNING | High temperature detected | code=W07 ", " ERROR | Communication timeout | code=E99 ",
@@ -16,13 +17,43 @@ clean_string5 = raw_logs[4].strip()
 # print(clean_string5.lower())
 
 # 2 task
-print(clean_string1.lower().split("|"))
-print(clean_string2.lower().split("|"))
-print(clean_string3.lower().split("|"))
-print(clean_string4.lower().split("|"))
-print(clean_string5.lower().split("|"))
+# print(clean_string1.lower().split("|"))
+# print(clean_string2.lower().split("|"))
+# print(clean_string3.lower().split("|"))
+# print(clean_string4.lower().split("|"))
+# print(clean_string5.lower().split("|"))
 
 # 3 task
+raw_logs_final=(clean_string1.lower().split("|") + clean_string2.lower().split("|") + clean_string3.lower().split("|")+ clean_string4.lower().split("|") +
+                clean_string5.lower().split("|"))
+print(raw_logs_final)
+
+list="".join(raw_logs_final)
+print(list)
+
+if "error" in list:
+    print("error found!")
+if list.startswith("error"):
+    print("error detected!")
+
+if "warning" in list:
+    print("warning found!")
+
+if "info" in list:
+    print("info found!")
+
+# 4 task
+print("errors")
+a=list.count("error")
+print(a)
+print("warnings")
+b=list.count("warning")
+print(b)
+print("infos")
+c=list.count("info")
+print(c)
+
+# 5 task
 
 
 
