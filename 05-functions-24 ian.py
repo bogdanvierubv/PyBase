642bff5216cbf7475f0f0011f7392d70e9147c4c
bogdanvierubv <bogdan.vieru.bv@gmail.com>
2026-01-31 09:50:48 +0200

    new

--- 05-functions-24 ian.py
+++ 05-functions-24 ian.py
@@ -1,28 +1,29 @@
 import copy
 #
-# var1 = "hello"
-#
-# def change_variable(param1):
-#     param1= "this is not a hello."
-#
+var1 = "hello"
+
+def change_variable(param1):
+    param1= "this is not a hello."
+
 # #
-# # print(var1)
-# # var1= "cat"
+# print(var1)
+var1= "cat"
 #
-# # change_variable(var1)
-# # #  cand pasam o variabila unei functii face pass by value
-# # print(var1)
+change_variable(var1)
+# #  cand pasam o variabila unei functii face pass by value
+# print(var1)
 #
-# var2 = [30,100]
-# # def change_variable_list(param2):
-# #     param2. append(99)
-# # #     stergerea elementelor din lista
-# #     param2.clear()
-#
-# # print(var2)
-# # change_variable_list(var2)
+var2 = [30,100]
+def change_variable_list(param2):
+    param2.append(99)
+
+#     stergerea elementelor din lista
+#     param2.clear()
+
+print(var2)
+change_variable_list(var2)
 # # # pass by reference
-# # print(var2)
+print(var2)
 #
 #
 # # max si suma nr dintr-o lista
@@ -47,7 +48,7 @@ import copy
 # print(pet_max(lista2))
 
 
-print("========Dictionar=========")
+# print("========Dictionar=========")
 # un dictionar este definit prin acolade
 student = {
     "name" : "Adrian",
@@ -62,17 +63,17 @@ student = {
 # print(student["name"])
 # print(student["backpack"][2])
 
-for k in student.keys():
-    print(k)
-
-for key, value in student.items():
-    print(key, "---", value)
-
-print(student)
-student["age"] =33
-print(student)
-student["address"]= "Brasov"
-print(student)
+# for k in student.keys():
+#     print(k)
+#
+# for key, value in student.items():
+#     print(key, "---", value)
+#
+# print(student)
+# student["age"] =33
+# print(student)
+# student["address"]= "Brasov"
+# print(student)
 
 # print(student["hhh"])- daca generam o eroare, arunca exceptie si opreste programul
 
@@ -87,86 +88,86 @@ print(student.get("hhh", "default value"))
 #  comanda clear sterge tot dictionarul
 # strudent.clear()
 
-if "address" in student:
-    print("Avem adresa pentru acest student!")
-else:
-    print("Atudentul acesta nu are adresa!")
+# if "address" in student:
+#     print("Avem adresa pentru acest student!")
+# else:
+#     print("Studentul acesta nu are adresa!")
+#
+# print(student)
+#
+# student_doi = student.copy()
+# # daca creem o functie de egalitate
+# student_doi["restante"] = 3
+# print("student original:")
+# print(student)
+# print("studentul doi cu restante:")
+# print(student_doi)
 
-print(student)
 
-student_doi = student.copy()
-# daca creem o functie de egalitate
-student_doi["restante"] = 3
-print("student original:")
-print(student)
-print("studentul doi cu restante:")
-print(student_doi)
-
-
-print("=======Shallow Copy Drawbaks========")
-
-student_doi["backpack"].append("casti")
-print(student_doi)
-print(student)
-#  reluare ora 11:27
-
-student_trei = copy.deepcopy(student)
-student_trei["backpack"].append("casti")
-print(student_trei)
-print("student original")
-print(student)
+# print("=======Shallow Copy Drawbaks========")
+#
+# student_doi["backpack"].append("casti")
+# print(student_doi)
+# print(student)
+# #  reluare ora 11:27
+#
+# student_trei = copy.deepcopy(student)
+# student_trei["backpack"].append("casti")
+# print(student_trei)
+# print("student original")
+# print(student)
 
 
 # creaza o functie care aduna toate numerele din dictionar
 
-dict2={
-    "name" : "Omega",
-    "dimensions" : 6,
-    "size" : 13,
-    "count" : -1,
-    "axis": "y",
-    "trees": "all"
-}
-
-def add_all_numbers(param1):
-    total = 0
-    for key in dict2.keys():
-        # print(dict2[key])
-        valoare = dict2[key]
-        if isinstance(valoare, int):
-            total = total + valoare
-    return total
-
-print(add_all_numbers(dict2))
-
-# check if var4 is integer
-var4 = 30.4
-print(isinstance(var4, float))
-
-# check if var4 is integer
-print(isinstance(var4,(int, float)))
-
-
-print("=====Odd/Even numbers====")
+# dict2={
+#     "name" : "Omega",
+#     "dimensions" : 6,
+#     "size" : 13,
+#     "count" : -1,
+#     "axis": "y",
+#     "trees": "all"
+# }
+#
+# def add_all_numbers(param1):
+#     total = 0
+#     for key in dict2.keys():
+#         # print(dict2[key])
+#         valoare = dict2[key]
+#         if isinstance(valoare, int):
+#             total = total + valoare
+#     return total
+#
+# print(add_all_numbers(dict2))
+#
+# # check if var4 is integer
+# var4 = 30.4
+# print(isinstance(var4, float))
+#
+# # check if var4 is integer
+# print(isinstance(var4,(int, float)))
+#
+#
+# print("=====Odd/Even numbers====")
 
 # 5 / 2 -> 2, rest 1
 # operator binar % - ce returneaza 1(inseamna ca numarul este impar) sau 0
 # print(5 % 2)
 
-def is_even(nr):
-    return nr % 2 == 0
+# def is_even(nr):
+#     return nr % 2 == 0
 
 # is_even(11)
-lista3 = [5, 10, 4, 30, 25, 7]
+# lista3 = [5, 10, 4, 30, 25, 7]
 
  # adunam toate numerele pare
 
-def add_all_evens(param1):
-    total = 0
-    for nr in param1:
-        if is_even(nr):
-            total = total + nr
-    return total
-
-rezultat = add_all_evens(lista3)
-print(rezultat)
+# def add_all_evens(param1):
+#     total = 0
+#     for nr in param1:
+#         if is_even(nr):
+#             total = total + nr
+#     return total
+#
+# rezultat = add_all_evens(lista3)
+# print(rezultat)
---
+++ 06-functions-27 ian.py
@@ -0,0 +1,73 @@
+# v1 = "Hello World"
+#
+# print(v1[0], v1[1], v1[-11])
+#
+# # Ce se printeaza?
+#
+# # 1. Eroare
+# # 2. "H l o"
+# # 3. Nimic
+#
+#
+# # Exemplul 2
+#
+# x1 = 10
+#
+# def change (x1):
+#     x1 = 11
+#
+# change(x1)
+# print(x1)
+
+
+print ("======Curs functii 06=======")
+
+# Ex:
+#  primim o lista de numere intregi. Separati-le in numere pare si impare, si salvatile intr-un dictionar.
+
+# ex dictionar = {
+#     "odd_numbers": [3, 5, 11, 13, 201],
+#     "even_numbers": [2, 4, 8, 10, 12, 200, 340],
+#     "odd_total": 233,
+#     "even_total": 576
+# }
+
+def is_even(nr):
+    return nr % 2 == 0
+
+def list_total(list1):
+    total = 0
+    for n in list1:
+        total = total + n
+    return total
+
+
+result = {}
+odd_list = []
+even_list = []
+
+
+for n in list1:
+
+        if is_even(n):
+
+            even_list.append(n)
+
+        else:
+
+            odd_list.append(n)
+
+# print(odd_list)
+# print(even_list)
+
+
+
+    result["odd_numbers"]= odd_list
+    result["even_list"]= even_list
+    result["odd_total"]= list_total(odd_list)
+    result["even_total"]= list_total(even_list)
+    return result
+
+data_list = [0, 5, 11, 31, 40, 52, 100, 999]
+var2 = process_data(data_list)
+print(var2)
